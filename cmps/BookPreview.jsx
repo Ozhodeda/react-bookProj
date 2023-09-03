5a41b77fcd62d253f4adbb881cad5e4ec3b9e598
Ozhodeda <oz36002@gmail.com>
2023-09-03 21:39:00 +0300

    finish part 1

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -3,7 +3,7 @@ export function BookPreview({book}){
         <article className="book-preview">
           {/* {  console.log('book:',book)} */}
         <h2>Book Title: {book.title}</h2>
-        <h4>Book Price: {book.listPrice['amount']}</h4>
+        <h4 >Book Price: {book.listPrice['amount']}</h4>
         <img src={`../BooksImages/${book.id}.jpg`} alt="BookImg" />
     </article>
     )
--- pages/BookDetails.jsx
+++ pages/BookDetails.jsx
@@ -6,17 +6,45 @@ export function BookDetails({ bookId, onBack }) {
 
     const [book, setBook] = useState(null)
 
-    useEffect(()=>{
+    useEffect(() => {
         bookService.get(bookId).then(setBook)
     }, [])
+    function pageCount(book) {
+        if (book.pageCount > 500) return 'Serious Reading'
+        if (book.pageCount > 200) return 'Descent Reading'
+        if (book.pageCount < 200) return 'Light Reading'
+    }
+    function bookAge(book) {
+        if (book.publishedDate < 10) return 'New'
+        if (book.publishedDate > 10) return 'Vintage'
+    }
 
-    if (!book) return <div>Loading...</div>
-    return (
-        <section className="book-details">
-            <h1>Book Title: {book.title}</h1>
-            <h1>Book Price: {book.listPrice['amount']}</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim rem accusantium, itaque ut voluptates quo? Vitae animi maiores nisi, assumenda molestias odit provident quaerat accusamus, reprehenderit impedit, possimus est ad?</p>
-            <button onClick={onBack}>Back</button>
-        </section>
-    )
-}
+    function getPriceColor(book) {
+        if (book.listPrice['amount'] > 100) return 'red'
+        if (book.listPrice['amount'] < 50) return 'green'
+        else return ''
+    }
+
+    function onSale(book){
+        if(book.listPrice['isOnSale']) return 'On Sale'
+    }
+  
+        if (!book) return <div>Loading...</div>
+        return (
+            <section className="book-details">
+                <h2>Book Title: {book.title}</h2>
+                <h3>Book SubTitle: {book.subtitle}</h3>
+                <h4>Book Authors: {book.authors}</h4>
+                <h4>Published Date: {book.publishedDate}</h4>
+                <h4>Book Age: {bookAge(book)}</h4>
+                <h4>Page Count: {book.pageCount}</h4>
+                <h4>Read Level: {pageCount(book)}</h4>
+                <h4>Book Category: {[book.categories]}</h4>
+                <h4 className={getPriceColor(book)}>Book Price: {book.listPrice['amount']}</h4>
+                <h4>Discount: {onSale(book)}</h4>
+                <h4>Book language: {book.language}</h4>
+                <p>Book Description :{book.description}</p>
+                <button onClick={onBack}>Back</button>
+            </section>
+        )
+    }
