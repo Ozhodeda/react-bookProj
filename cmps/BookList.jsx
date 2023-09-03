992ba07c612a086fb25878547ea756a27f16a9eb
Ozhodeda <oz36002@gmail.com>
2023-09-03 16:55:47 +0300

    remove book

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -1,13 +1,13 @@
 import { BookPreview } from "./BookPreview.jsx"
 
-export function BookList({ books, onREmoveBook }) {
+export function BookList({ books, onRemoveBook }) {
     return (
         <ul className="book-list">
             {books.map(book => (
                 <li key={book.id}>
                     <BookPreview book={book} />
                     <section>
-                        <button onClick={() => onREmoveBook(book.id)}>Remove Book</button>
+                        <button onClick={() => onRemoveBook(book.id)}>Remove Book</button>
                     </section>
                 </li>
             ))}
